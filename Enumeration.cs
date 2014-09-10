d78ee4b6fd1d6ec7a0194599fdb6947b86fd6b16
Chris Missal <chris.missal@gmail.com>
2014-09-10 00:32:17 -0500

    add compare tests

--- Enumeration.cs
+++ Enumeration.cs
@@ -64,12 +64,7 @@ namespace Headspring
 
         public int CompareTo(TEnumeration other)
         {
-            if (other == null)
-            {
-                Value.CompareTo(other);
-            }
-
-            return Value.CompareTo(other.Value);
+            return Value.CompareTo(other == default(TEnumeration) ? default(TValue) : other.Value);
         }
 
         public override sealed string ToString()
